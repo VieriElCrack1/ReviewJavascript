d6963106b747fa5ce01abcb241cdc739b72784d9
VieriElCrack1 <santosvieri9@gmail.com>
2025-08-04 21:41:37 -0500

    feat: Fetch Api.

---
+++ src/review/10-fetch-api.js
@@ -0,0 +1,15 @@
+const apiKey = 'd45Gotd7Esfwk5kPI53yZZpQ9wHh8Z8j';
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+peticion
+    .then( resp => resp.json() )
+    .then( ( { data } ) => {
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append( img );
+    })
+    .catch( console.warn );
